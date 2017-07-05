651cbbd7af5d83f5d4b78c2713f16dd422d6b8a4
efeeley <elisabeth.k.feeley@gmail.com>
2017-07-05 15:49:55 -0400

    Add project files.

---
+++ HW2-3_EKF/Message.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**************************************
+ * Name: Elisabeth Feeley
+ * Date: 7/11/2012
+ * Assignment: HW2-3_EKF (Message)
+ * Resources: N/A
+ * ***********************************/
+
+namespace HW2_3_EKF
+{
+    class Message
+    {
+        //Attributes
+        private string messageTape, messageTime, messageNumber;
+
+        //Methods
+        public string deleteMessage()
+        {
+            return "You have deleted: " + getMessageNumber() + " from " + getMessageTape();
+        }
+
+        public string saveMessage()
+        {
+            return getMessageNumber() + " recieved on " + getMessageTime() + " has been saved on " + getMessageTape();
+        }
+
+        public string playMessage()
+        {
+            return "Playing message number " + getMessageNumber() + " recieved on " + getMessageTime();
+        }
+
+        //Gets
+        public string getMessageTape()
+        {
+            return messageTape;
+        }
+        public string getMessageTime()
+        {
+            return messageTime;
+        }
+        public string getMessageNumber()
+        {
+            return messageNumber;
+        }
+
+        //Sets
+        public void setMessageTape(string aMessageTape)
+        {
+            messageTape = aMessageTape;
+        }
+        public void setMessageTime(string aMessageTime)
+        {
+            messageTime = aMessageTime;
+        }
+        public void setMessageNumber(string aMessageNumber)
+        {
+            messageNumber = aMessageNumber;
+        }
+
+    }//end class Message
+}//end namespace HW2-3_EKF
---
+++ HW2-3_EKF/TestMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**************************************
+ * Name: Elisabeth Feeley
+ * Date: 7/11/2012
+ * Assignment: HW2-3_EKF (Message)
+ * Resources: N/A
+ * ***********************************/
+
+namespace HW2_3_EKF
+{
+    class TestMessage
+    {
+        static void Main(string[] args)
+        {
+            Message m1 = new Message();
+
+            m1.setMessageNumber("M42");
+            m1.setMessageTime("July 11, 2012 10:45 PM");
+            m1.setMessageTape("Tape 3");
+
+            Console.WriteLine(m1.saveMessage());
+            Console.ReadLine();
+
+            Console.WriteLine(m1.playMessage());
+            Console.ReadLine();
+
+            Console.WriteLine(m1.deleteMessage());
+            Console.ReadLine();
+        }//end static void main
+    }//end class TestMessage
+}//end namespace HW2-3_EKF
